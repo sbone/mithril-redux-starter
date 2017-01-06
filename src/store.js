f483521b2360d8274ba4600d44ae213700b9d24d
Steven Bone <steven@stevenbone.com>
2017-01-06 13:56:50 -0500

    setup for Redux Dev Tools

--- src/store.js
+++ src/store.js
@@ -1,10 +1,9 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { redrawMiddleware } from 'mithril-redux';
 import rootReducer from './reducers';
 
 export default function configureStore(initialState) {
-  const createModifiedStore = applyMiddleware(
-    redrawMiddleware
-  )(createStore);
-  return createModifiedStore(rootReducer, initialState);
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const createModifiedStore = applyMiddleware(redrawMiddleware)(createStore);
+  return createModifiedStore(rootReducer, initialState, composeEnhancers(applyMiddleware(...redrawMiddleware)));
 }
