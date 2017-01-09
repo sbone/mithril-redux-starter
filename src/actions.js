96c79f0e7c0483c928071d9cc820240299bbeba7
Steven Bone <steven@stevenbone.com>
2017-01-09 10:30:49 -0500

    remove unused original components and parts

--- src/actions.js
+++ src/actions.js
@@ -1,26 +1,4 @@
-export const INCREMENT_AGE = 'INCREMENT_AGE';
-export const DECREMENT_AGE = 'DECREMENT_AGE';
-export const RESET_AGE = 'RESET_AGE';
 export const TOGGLE_SIDEBAR = 'TOGGLE_SIDEBAR';
-
-export function incrementAge() {
-  return {
-    type: INCREMENT_AGE
-  };
-}
-
-export function decrementAge() {
-  return {
-    type: DECREMENT_AGE
-  };
-}
-
-export function resetAge() {
-  return {
-    type: RESET_AGE,
-    age: 30
-  };
-}
 
 export function openSidebar() {
   return {
